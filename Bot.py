01ba847e48c23170aa6f7f8b1c251db86a6559cf
joao-0213 <76481873+joao-0213@users.noreply.github.com>
2020-12-22 17:34:54 -0300

    arredondando cooldown

--- Bot.py
+++ Bot.py
@@ -2,44 +2,39 @@ import discord
 from Dataclasses import SingleGuildData
 from typing import Optional
 import yaml
-import asyncio
 from discord.ext import commands
 
 with open('credentials.yaml') as t:
     credentials = yaml.load(t, Loader=yaml.FullLoader)
-
+    print (credentials.get("PREFIXO"))
 client = commands.Bot(command_prefix=credentials.get("PREFIXO"), case_insensitive=True)
 
 @client.event
 async def on_ready():
-    print("Bot pronto.")
+    print('Bot pronto')
+
 
 @client.event
-async def on_message(message: discord.Message):
+async def on_message(message):
     # canal para o qual vai ser enviado o log da mensagem DM
     el = SingleGuildData.get_instance().channel
 
     # verifica se o canal de envio foi escolhido, se a mensagem é na DM e envia um embed para o canal escolhido
     if message.guild == None and not el == None and not message.author.bot:
-        embed = discord.Embed(title="Mensagem enviada para a DM do bot", 
-        description= message.content, 
-        color=0xff0000)
+        embed = discord.Embed(title="Mensagem enviada para a DM do bot", description= message.content, color=0xff0000)
         embed.set_author(name= message.author.name, icon_url= message.author.avatar_url)
         await el.send(embed=embed)
 
-    if client.user.mentioned_in(message):
-        await message.channel.send(f"{message.author.mention} oi hihihi, meu prefixo é `{credentials.get('PREFIXO')}`.")
-
     await client.process_commands(message)
+
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         await ctx.send(
-            f"{ctx.author.mention} Pare. Pare imediatamente de executar este comando, ainda falta {error.retry_after} para você "
+            f"{ctx.author.mention} Pare. Pare imediatamente de executar este comando. Ainda faltam {int(round(error.retry_after,0))}s para você "
             "usar o comando novamente."
-            )
-
+        )
 
 @client.command(pass_context=True)
 async def ping(ctx):
@@ -54,24 +49,26 @@ async def dm(ctx, user: discord.Member, *, msg: str):
        é necessário de que a DM dela esteja aberta.
        """
     await user.send(msg)
-    await user.send(embed=discord.Embed(title="Responda seu amigo (ou inimigo) anônimo!", 
-        description="Para responder use `,responder <mensagem>`", 
-        color=0xff0000))
+    await user.send(embed=discord.Embed(title="Responda seu amigo (ou inimigo) anônimo!",
+                                        description="Para responder use `,responder <mensagem>`",
+                                        color=0xff0000))
+    await ctx.message.delete()
 
     def check(message):
-        msgcon =  message.content.startswith(f"{credentials.get('PREFIXO')}responder")
+        msgcon = message.content.startswith(f"{credentials.get('PREFIXO')}responder")
         return message.author.id == user.id and message.guild is None and msgcon
 
     # como levar ratelimit passo-a-passo
     try:
-        message = await client.wait_for("message", 
-        check=check, 
-        timeout=30.0)
+        message = await client.wait_for("message",
+                                        check=check,
+                                        timeout=300.0)
     except asyncio.TimeoutError:
+        await user.send("Oh não! VocÊ demorou muito para responder. :sad:")
         pass
     else:
         con = " ".join(message.content.split(" ")[1:])
-        
+
         embed = discord.Embed(
             title=f"E ele respondeu!",
             color=discord.Color.red(),
@@ -81,7 +78,8 @@ async def dm(ctx, user: discord.Member, *, msg: str):
         embed.set_author(name=str(user), icon_url=message.author.avatar_url)
         await ctx.author.send(embed=embed)
 
-    await ctx.message.delete()
+
+
 
 @client.command()
 # estranho
