30513129efbb1dc1e2f156389083a830167ffe58
miguel <rodoviabr102@gmail.com>
2021-03-08 12:05:10 -0300

    Arrumado o código do naegi grrr

--- ext/audio.py
+++ ext/audio.py
@@ -5,56 +5,78 @@ import ffmpeg
 import yaml
 import youtube_dl
 from discord import FFmpegPCMAudio
-from discord.utils import get
-from requests import get
 
+youtube_dl.utils.bug_reports_message = lambda: ''
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
 
-@client.event
-async def on_ready():
-    print('Script de música iniciado')
+ffmpeg_options = {
+    'options': '-vn'
+}
 
+youtube = youtube_dl.YoutubeDL(ytdl_format_options)
 
-def search(query):
-    with youtube_dl.YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
-        if query.startswith("www.") or query.startswith("https:"):
-            info = ydl.extract_info(query, download=False)
-        else:
-            info = ydl.extract_info("ytsearch:{}".format(query), download=False)['entries'][0]
-        video, source = (info, info['formats'][0]['url'])
-        return source
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
 
-@client.command(aliases=["p", "pl"])
-async def play(ctx, *, query):
-    channel = ctx.message.author.voice.channel
-    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    voice = channel.connect()
-    
-    
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice == None:
-      await voice
-    else:
-        pass
-    
-        voice.play(FFmpegPCMAudio(search(query), **FFMPEG_OPTS), after=lambda e: ctx.send('Música', e))
-        voice.is_playing()
-    
+        self.data = data
         
-          
-    
-    
-            
-@client.command(aliases=["lv", "disconnect"])
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    await ctx.voice_client.disconnect()
-    if voice_client:
-        await voice_client.disconnect()
-    else:
-        await ctx.send('Eu não estou em um canal de voz')
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda : youtube.extract_info(url=url, download=not stream))
+
+        if "entries" in data:
+            # estamos com uma playlist, e vamos pegar só o primeiro vídeo.
+            data = data["entries"][0]
+        filename = data["url"] if stream else youtube.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 class Audio(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @commands.command(aliases=["p", "pl"])
+    async def play(self, ctx, *, query: str):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(query)
+            ctx.voice_client.play(player, after=lambda err : print(f"Erro no player: {err}"))
+
+        await ctx.reply(f"Ouvindo e tocando: {player.title}")
+
+    @commands.command(aliases=["lv", "disconnect"])
+    async def leave(self, ctx):
+        voice_client = ctx.voice_client
+        if voice_client:
+            await voice_client.disconnect()
+        else:
+            await ctx.reply('Eu não estou em um canal de voz')
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                return await ctx.reply("Você não está conectado em um canal de voz.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+def setup(client):
+    client.add_cog(Audio(client))
